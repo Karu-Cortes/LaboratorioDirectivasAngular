267783322bf455ac2195e314e0f63b217dc7e2de
KarenCortes <karenj.cortesn@gmail.com>
2024-02-08 08:52:54 -0500

    directivas

---
+++ angular-directives/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { ResaltarParesDirective } from './resaltar-pares.directive';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public muestras = [
+    'Muestra 1',
+    'Muestra 2',
+    'Muestra 3',
+    'Muestra 4',
+    'Muestra 5',
+  ];
+}
